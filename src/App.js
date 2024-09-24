23dada9fa5b07bb4ff21b2351aa71b3b277a6650
unknown <hirk09@gmail.com>
2024-09-24 20:40:32 +0530

    styling css

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ function App() {
   const [text, setText] = useState(''); // Storing input data
 
   const handleSubmit = () => {} // Handle value after submiting input text
-  
+
   return (
     <div className="app">
       <div className='conatiner'>
       <div className='title'>String Calculator TDD Kata</div>
       <form onSubmit={handleSubmit}>
+        <div className='form-field'>
                 <input
                     value={text}
                     onChange={(e) => setText(e.target.value)}
                     rows="4"
                     className='input-field'
                     placeholder="Enter number here"
-                />
-                <br />
+                    />
+                    </div>
+                <div className='btn'>
                 <button type="submit">Submit</button>
+                </div>
             </form>
       </div>
     </div>
