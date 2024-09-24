7d50b6eb11049a9d7f9617a8cb6dcdd973da45b4
unknown <hirk09@gmail.com>
2024-09-24 20:12:20 +0530

    setup project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className='conatiner'>
+      <div className='title'>String Calculator TDD Kata</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
