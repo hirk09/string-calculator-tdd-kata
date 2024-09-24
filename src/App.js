6b1224c259217afd5697a5903950e4f776e796a0
unknown <hirk09@gmail.com>
2024-09-24 20:33:32 +0530

    implementeed input field with button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState(''); // Storing input data
+
+  const handleSubmit = () => {} // Handle value after submiting input text
+  
   return (
     <div className="app">
       <div className='conatiner'>
       <div className='title'>String Calculator TDD Kata</div>
+      <form onSubmit={handleSubmit}>
+                <input
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    rows="4"
+                    className='input-field'
+                    placeholder="Enter number here"
+                />
+                <br />
+                <button type="submit">Submit</button>
+            </form>
       </div>
     </div>
   );
